3449ee7c84b1357e4e8f78281cfc9906271a2510
christianchiarulli <chrisatmachine@gmail.com>
2018-06-25 17:22:50 -0400

    added some things to Basics

--- InstallBasics.sh
+++ InstallBasics.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-sudo apt install -y vim i3 htop calcurse sl cowsay cmatrix espeak tmux xcompmgr
-sudo apt install -y compton 
-sudo apt install -y nitrogen feh
+sudo apt install -y curl
+sudo apt install -y cmake
+sudo apt install -y emacs
+sudo apt install -y vim 
+sudo apt install -y compton
+sudo apt install -y i3
+sudo apt install -y htop
+sudo apt install -y glances
+sudo apt install -y calcurse
+sudo apt install -y sl
+sudo apt install -y cowsay
+sudo apt install -y cmatrix
+sudo apt install -y espeak
+sudo apt install -y tmux
+sudo apt install -y git
+sudo apt install -y nitrogen 
+sudo apt install -y feh
 sudo apt install -y redshift
 sudo apt install -y default-jre
 sudo apt install -y default-jdk
@@ -16,8 +30,14 @@ sudo apt install -y bluez
 sudo apt install -y bluez-tools
 sudo apt install -y blueman
 sudo apt install -y rfkill
+sudo apt install -y terminator
+sudo apt install -y apt-transport-https
 sudo apt install -y openssh-server
+# for irony server in emacs
+sudo apt install -y libclang-dev
 # for some reason I can't pip3 install this in my virtualenv
-#sudo apt install -y python3-tk
+sudo apt install -y python3-tk
+sudo apt install -y virtualenv
+sudo apt install -y docker-ce
 sudo apt update 
 sudo apt upgrade -y
